6503703180872df01cc78d637361e8785ebbffa0
Ankush Amar <ankushamar018@gmail.com>
2026-08-29 17:23:06 +0530

    29-aug-26

--- handeler.js
+++ handeler.js
@@ -1,5 +1,21 @@
 const requestHandeler = (req , res) => {
     console.log(req.url, req.method);
 
+    if(req.url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+            <html>
+                <head>
+                    <title>Keep Practicing</title>
+                </head>
+                <body>
+                    <h1>Calculator Home</h1>
+                    <a href="/calculator">Go to Calculator</a>
+                <body>
+            </html>
+        `);
+        return res.end();
+    }
+
 }
 exports.requestHandeler = requestHandeler;
