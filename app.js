bb108b237d5a5bd7a02e0478f33e710bb7e3082c
Ankush Amar <ankushamar018@gmail.com>
2026-08-29 16:47:47 +0530

    29-aug-26

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.header, req.method);
+    console.log(req.url, req.method);
+});
+
+const PORT = 3000;
+server.listen(PORT , () => {
+    console.log(`Server running on adderess http://localhost:${PORT}`);
 });
